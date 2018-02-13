a9dd39da6822592524443a331ce8982ca745a27a
inyounkyo <inyounkyo@hanmail.net>
2018-02-13 17:26:50 +0900

    first commit

--- src/main/java/kr/go/LibApplication.java
+++ src/main/java/kr/go/LibApplication.java
@@ -3,6 +3,8 @@ package kr.go;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
@@ -14,9 +16,18 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @MapperScan("kr.go")
 @EnableCaching
 @EnableAspectJAutoProxy
-public class LibApplication {
+public class LibApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibApplication.class, args);
 	}
+
+	/**
+	 * Used when run as WAR
+	 */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(LibApplication.class);
+	}
+
 }
